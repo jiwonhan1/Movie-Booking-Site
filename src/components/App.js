07276e1aee8df9aa8ad3fb59554923ac87d596e0
Jiwon <jiwon1.han@gmail.com>
2020-04-24 12:48:11 -0700

    Installed axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,58 @@
 import React, {Component} from 'react';
+import Movie from "/Movie";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isLoading: true,
-      masterMovieList = [],
+      masterMovieList: [],
       showTimes: ['3PM', '8PM']
-    };
-
-    getMovies = async () => {
-      const {
-        data: {
-          masterMovieList
-        }
-      } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-      this.setState({masterMovieList, isLoading: false});
-    };
-
-    componentDidMount() {
-      this.getMovies();
-    };
-
-    render() {}
-
+    }
   }
-  return (
-    
-  );
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { masterMovieList }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ masterMovieList, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, masterMovieList } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {masterMovieList.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Movie.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie(prop) {
+  return (
+    <>
+    <p>{props.id}</p>
+    <p>{props.title}</p>
+    </>
+  )
+}
